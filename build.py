845bad437938eec5984afb575c6f0339f4d4989c
Rozhbas Leader Blade Never Die <103634965+RozhBasXYZ@users.noreply.github.com>
2024-06-14 12:56:54 +0700

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import os,sys,shutil
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-main_file = "blade.cpp"
+main_file = "bladevip.cpp"
 try:
 	nama = main_file.split(".")[0]
 	setup(
