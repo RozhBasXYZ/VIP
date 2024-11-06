3e4be91bffc52a7218c541bea5bb6595bcfb6b5e
Rozhbas Leader Blade Never Die <103634965+RozhBasXYZ@users.noreply.github.com>
2024-11-06 17:12:48 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 import os, blade as rozh
 try: os.system("git pull"); os.system("python3.10 build.py")
 except: print("[!] anda memakai versi terbaru")
+folder = ["OK","CP"]
+for x in folder:
+	try: os.mkdir("/sdcard/FBL {}".format(x))
+	except Exception as e: pass
 rozh.main_line()
